ee35049cf68b5826cea5207423f954d3d63e8fcf
Orla Collins <ocollin2@terpmail.umd.edu>
2023-11-17 10:40:42 -0500

    added code to get a pun back from the API

--- getting_puns.py
+++ getting_puns.py
@@ -1,1 +1,10 @@
 # download and print puns from an API
+
+import requests
+import json
+
+# download a random pun from https://www.punapi.rest/
+pun_request = requests.get("https://www.punapi.rest/api/pun")
+pun_data = json.loads(pun_request.content)
+
+print(pun_data)
